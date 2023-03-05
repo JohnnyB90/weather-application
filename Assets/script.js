88638c474ed819877c1a7470feb3068d48e6968d
Johnny <117334322+JohnnyB90@users.noreply.github.com>
2023-03-04 21:00:08 -0500

    Added an automatic geolocator for current city weather for user

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,19 @@
-
-fetch("https://api.openweathermap.org/data/2.5/forecast?lat=35.2270&lon=-80.8431&appid=015b0ce1972981e980b4d813137568cb"
+  // Get the user's current position using the Geolocation API
+  navigator.geolocation.getCurrentPosition(function(position) {
+    // Get the latitude and longitude from the position object
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+fetch("https://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lon+"&appid=015b0ce1972981e980b4d813137568cb"
 )
 .then(response => response.json())
 .then(data => {
   // Displaying available data to console
   console.log(data);
   // Displaying five day forecasts to console
-  const forecastData = data.list.splice(0, 5); 
+  var forecastData = data.list.splice(0, 5); 
   console.log(forecastData)
   // Pulling out the current city based on long/lat coordinates
-  const currentCity = data.city.name;
+  var currentCity = data.city.name;
   var currentCityDiv = document.getElementById("current-city")
   currentCityDiv.textContent = currentCity;
   currentCityDiv.style.fontWeight = "bold";
@@ -24,9 +28,9 @@ fetch("https://api.openweathermap.org/data/2.5/forecast?lat=35.2270&lon=-80.8431
   // Retrieving weather data for the current time
   const currentWeatherData = forecastData[0];
   // Retrieving wind speed, temperature, and humidity from current weather data
-  const windSpeed = currentWeatherData.wind.speed;
-  const temperatureInKelvin = currentWeatherData.main.temp;
-  const temperatureInFahrenheit = Math.round((temperatureInKelvin - 273.15) * 9/5 + 32);
+  var windSpeed = currentWeatherData.wind.speed;
+  var temperatureInKelvin = currentWeatherData.main.temp;
+  var temperatureInFahrenheit = Math.round((temperatureInKelvin - 273.15) * 9/5 + 32);
   const humidity = currentWeatherData.main.humidity;
   // Displaying wind speed, temperature, and humidity in HTML
   var windDiv = document.getElementById("wind");
@@ -39,4 +43,5 @@ fetch("https://api.openweathermap.org/data/2.5/forecast?lat=35.2270&lon=-80.8431
 // Error catch that will display if something is not working properly
 .catch(error => {
   console.log("Unable to fetch that!")
+});
 });
